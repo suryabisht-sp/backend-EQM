64e66730839068249daaea98f2ba69ec6050236e
suryabisht-sp <surya_bisht@softprodigy.com>
2024-06-26 17:21:40 +0530

    modifications done

--- EQM/myapp/urls.py
+++ EQM/myapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('addData/',addData.as_view),
+    path('addData/',addData.as_view()),
     path('predict_expense/', predict_expense.as_view()),
     path("data", PredictExpenses.as_view()),
     path("predict_expense_regression/", predict_expense_regression.as_view())
